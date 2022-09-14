95cdf86e1b61e13f0cfa83c5405ed294473a0212
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-09-13 21:05:41 -0300

    configuring Tailwind and applying the styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
+import './styles/main.css';
+import logo from './assets/Logo.svg'
 
 export function App() {
 
-
   return (
-    <div className="App">
-      
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center m-20'>
+      <img className='' src={logo} />
     </div>
   )
 }
