9c49f08ef26a93b67044dd0b45131ff0be9fa19b
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-09-19 15:32:11 -0300

    We develop Modal and its features

--- src/components/Form/input.tsx
+++ src/components/Form/input.tsx
@@ -7,7 +7,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 export function Input(props: InputProps) {
     return (
         <input {...props}
-        className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+        className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 focus: outline-none'
   />
     )
 }
